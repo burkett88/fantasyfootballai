a7741466c9987f3f362cac533f17f482022eb594
burkett88 <14807045+burkett88@users.noreply.github.com>
2025-08-24 12:31:25 -0700

    improved signature

--- main.py
+++ main.py
@@ -11,24 +11,24 @@ def main():
 
     load_dotenv()
 
+
     lm = dspy.LM("openai/gpt-4o-search-preview-2025-03-11", api_key=os.getenv("OPENAI_API_KEY"), temperature=None)
     dspy.configure(lm=lm)
 
-    class fantasyFootballSignature(dspy.Signature):
-        """This tool is a researcher for players in the 2025 fantasy football season"""
+    class fantasyFootballPlayerResearcher(dspy.Signature):
+        """This is a very detailed report for football players in the 2025 fantasy football season. This summarizes data from data-driven sources of fantasy football knowledge and not just mainstream sources like cbs."""
         playerName: str = dspy.InputField(desc="Fantasy Football Player Name")
         playing_time: str = dspy.OutputField(desc="Indications about how this players snap count may compare vs the previous season.")
-        injury_risk: str = dspy.OutputField(desc="Research on the injury risk for this player in 2025")
-        breakout_risk: str = dspy.OutputField(desc="Research on whether people expect this player to potentially breakout, which we define as a significant improvement to their per game points")
+        injury_risk: str = dspy.OutputField(desc="Research on the injury risk for this player in 2025. Consider their injury history and usage.")
+        breakout_risk: str = dspy.OutputField(desc="Research on whether people expect this player to potentially breakout, which we define as a significant improvement to their per game points. Include direct quotes where appropriate.")
         bust_risk: str = dspy.OutputField(desc="Research on whether this player could be a bust, particularly relative to where they are being drafted")
         key_changes: str = dspy.OutputField(desc="Personnel changes or coaching changes on the team that may affect their playing time or effectiveness")
         outlook: str = dspy.OutputField(desc="An overall assessment of their outlook")
 
-    guru = dspy.Predict(fantasyFootballSignature)
+    playerResearcher = dspy.Predict(fantasyFootballPlayerResearcher)
 
-    player_data = guru(playerName=args.player_name)
+    player_data = playerResearcher(playerName=args.player_name)
     print (player_data)
-    #create_player_card(args.player_name, player_data)
 
 if __name__ == "__main__":
     main()
